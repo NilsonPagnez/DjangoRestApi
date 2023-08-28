c1eec453de686eb7752c0a368df5652d0cd972c9
Nilson Pagnez <nilson.pagneto@gmail.com>
2023-08-28 13:53:47 -0300

    estudando api rest

---
+++ escola/serializer.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from escola.models import Aluno, Curso
+
+
+class AlunoSerializer (serializers.ModelSerializer):
+    class Meta :
+        model = Aluno
+        fields = ['id', 'nome', 'rg', 'cpf', 'data_nascimento']
+
+
+class CursoSerializer
+    class Meta:
+        model = Curso
+        field = '__all__'
