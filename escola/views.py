dcb15030c98552be52c2708bd1adb33d352b8321
Nilson Pagnez <nilson.pagneto@gmail.com>
2023-08-30 13:36:51 -0300

    api pronta com crud de alunos e matriculas

--- escola/views.py
+++ escola/views.py
@@ -1,7 +1,39 @@
-from django.http import JsonResponse
+from rest_framework import viewsets, generics
+from escola.models import Aluno, Curso, Matricula
+from escola.serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculaAlunoSerializer
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 
-def alunos(request):
-    if request.method == 'GET':
-        aluno = {'id': 1, 'nome': 'Nilson'}
-        return JsonResponse(aluno)
+class AlunosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os alunos e alunas"""
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+
+class CursosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os cursos"""
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+
+class MatriculasViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os Matriculas"""
+    queryset = Matricula.objects.all()
+    serializer_class = MatriculaSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+
+class ListaMatriculaAluno(generics.ListAPIView):
+    """Listando as matrículas de um aluno ou aluna"""
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(aluno_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaMatriculaAlunoSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
--- setup/urls.py
+++ setup/urls.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
-from django.urls import path
-from escola.views import alunos
+from django.urls import include, path
+from rest_framework import routers
+from escola.views import AlunosViewSet, CursosViewSet, MatriculasViewSet, ListaMatriculaAluno
+
+router = routers.DefaultRouter()
+router.register('alunos', AlunosViewSet, basename='Alunos')
+router.register('cursos', CursosViewSet, basename='Cursos')
+router.register('matriculas', MatriculasViewSet, basename='Cursos')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('alunos/', alunos),
+    path('', include(router.urls)),
+    path('aluno/<int:pk>/matriculas/', ListaMatriculaAluno.as_view())
 ]
